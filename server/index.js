872e88eb3d2e9cf9e59a520de04aa8a059c47ec8
Josi Reis-West <josialexa85@gmail.com>
2019-12-02 16:17:01 -0600

    Completed step 3

--- server/index.js
+++ server/index.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+let products = require('../products.json');
+
+app.get('/api/products', (req, res) => {
+    res.status(200).send(products);
+});
+
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
 });
